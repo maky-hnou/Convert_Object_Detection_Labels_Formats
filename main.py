f23a3006ad44a29dc7f595a6f8a705a317ba3216
hani <haniyousfi@gmail.com>
2020-10-05 00:22:39 +0100

    Add yolo to pascal conversion option

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
+import sys
 
 from pascal_voc2yolo import Pascal2Yolo
+from yolo2pascal_voc import Yolo2Pascal
 
 if __name__ == '__main__':
     classes = input('[INFO]: Please type the categories of '
@@ -20,4 +22,11 @@ if __name__ == '__main__':
     if action == '1':
         pascal2yolo = Pascal2Yolo(classes=classes)
         for path in data_paths:
-            pascal2yolo.convert(path)
+            pascal2yolo.convert(path=path)
+    elif action == '2':
+        yolo2pascal = Yolo2Pascal(classes=classes)
+        for path in data_paths:
+            yolo2pascal.convert(path=path)
+    else:
+        print('Wrong choice')
+        sys.exit(0)
